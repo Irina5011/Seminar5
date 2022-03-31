75122c6975086fa1ec7a8a077ade78f52b253842
Irina <89231707647@mail.ru>
2022-03-31 11:33:17 +0700

    added files

---
+++ raznicaNexhdyMinIMax/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 3: Задайте массив вещественных чисел. Найдите разницу 
+// между максимальным и минимальным элементов массива.
+
+double[,] a = new double[5, 10];
+
+Random random = new Random();
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 10; j++)
+    {
+        a[i, j] = random.NextDouble() * 100; // NextDouble() дает случайное вещественное число в диапазоне от 0 до 1
+        Console.Write("{0,6:F2}", a[i, j]);
+    }
+    Console.WriteLine();
+}
+
---
+++ zapMassivSluchChislami/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 1: Задайте массив, заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+int[] RandArray(int lenght)
+{
+    int[] answer = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        answer[i] = new Random().Next(100, 1000);
+    }
+    return answer;
+}
+
+void PrintArray(int[] Mas)
+{
+    int count = Mas.Length;
+    for (int pos = 0; pos < count; pos++)
+    {
+        Console.Write(Mas[pos] + ", ");
+    }
+    Console.WriteLine();
+}
+
+int[] arr = RandArray(6);
+PrintArray(arr);
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] % 2 == 0)
+        count++;
+}
+Console.WriteLine($"Количество четных чисел в массиве => {count}");
+
